6f7e713c389fd39e94921ce4844fb057f5ac893c
Yuznstell <yudhanugraha.cr7@gmail.com>
2025-09-29 19:26:22 +0700

    updateeer

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,7 @@
 export default function Success() {
   return (
     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
-      <h1>✅ Pembayaran Berhasil!</h1>
+      <h1>✅Selamat Pembayaran Berhasil!</h1>
       <p>Ini halaman dummy untuk simulasi pembayaran sukses.</p>
     </div>
   )
