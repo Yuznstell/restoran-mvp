9e9dc1f6356587f7011357b738808e32e85b899a
Yuznstell <yudhanugraha.cr7@gmail.com>
2025-09-29 19:11:46 +0700

    updatee

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Snap.js Midtrans */}
-        <script
-          type="text/javascript"
-          src="https://app.sandbox.midtrans.com/snap/snap.js"
-          data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
-        ></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
